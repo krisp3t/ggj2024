ffa8c889731239fa984f9a56aba449c033aface1
krisp3t <77499501+krisp3t@users.noreply.github.com>
2024-01-26 22:08:21 +0100

    Add scenes

---
+++ scenes/Projectile.gd
@@ -0,0 +1,14 @@
+extends Node2D
+class_name Projectile;
+
+
+func _ready():
+	# self.set_freeze_enabled(true);
+	pass;
+
+
+func _process(delta):
+	pass
+
+func throw():
+	pass;
---
+++ scenes/Slingshot.gd
@@ -0,0 +1,44 @@
+extends Node2D
+
+enum State {
+	IDLE,
+	PULLING,
+	THROWN,
+	RESET
+}
+
+var state : State = State.IDLE;
+
+func _ready():
+	pass # Replace with function body.
+
+
+func _process(delta):
+	match state:
+		State.IDLE:
+			return;
+		State.PULLING:
+			if Input.is_action_pressed("LMouse"):
+				var mouse_pos := get_global_mouse_position();
+				if mouse_pos.distance_to(%SlingshotCenter.position) > 100:
+					mouse_pos = %SlingshotCenter.position + (mouse_pos - %SlingshotCenter.position).normalized() * 100;
+				%LeftLine.points[1] = mouse_pos;
+				%RightLine.points[1] = mouse_pos;
+			else:
+				var location := get_global_mouse_position();
+				var distance := location.distance_to(%SlingshotCenter.position);
+				var velocity = %SlingshotCenter.position - location;
+				var player : Projectile = get_tree().get_nodes_in_group("Player")[0];
+				player.throw();
+				player.apply_impulse(Vector2(), velocity / 50 * distance);
+				state = State.THROWN;
+				
+		State.THROWN:
+			return;
+		State.RESET:
+			return;
+	
+
+
+func _on_touch_area_input_event(viewport, event, shape_idx):
+	state = State.PULLING;
