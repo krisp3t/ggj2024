b398eae97b37528635d768854bbd60cf99771164
NebulaeOfnir <118994104+NebulaeOfnir@users.noreply.github.com>
2024-01-27 19:57:37 +0100

    Revert "Resolve merge conflict"
    This reverts commit 0f79957ab9a9d2589a2afa202f7bc610f22f2af6, reversing
    changes made to 67ed4750b0a806f9f0a2deb4cc33049f9e6d9558.

--- scenes/Enemy.gd
+++ scenes/Enemy.gd
@@ -1,13 +1,9 @@
 extends RigidBody2D
 @export var sprite : SpriteFrames;
-@export var laugh_value : int
 
 func _ready():
 	set_freeze_enabled(true);
-<<<<<<< HEAD
 	
-=======
->>>>>>> 30ed24773fe84d516a86f17fcaed899c65f1cd7c
 	$AnimatedSprite2D.sprite_frames = sprite;
 	$AnimatedSprite2D.play();
 
