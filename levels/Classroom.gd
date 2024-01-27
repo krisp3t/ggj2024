2564f3b63753de057e0c75f403f5eb3a094d0fbd
krisp3t <77499501+krisp3t@users.noreply.github.com>
2024-01-27 20:51:49 +0100

    Add signals to Classroom

---
+++ levels/Classroom.gd
@@ -0,0 +1,17 @@
+extends Node2D
+signal shot
+
+func _ready():
+	pass # Replace with function body.
+
+func _process(delta):
+	pass
+
+func _on_slingshot_shot():
+	shot.emit();
+
+func _on_ground_area_body_entered(body):
+	if body is Projectile:
+		await get_tree().create_timer(0.5).timeout;
+		if is_instance_valid(body):
+			body.queue_free();
