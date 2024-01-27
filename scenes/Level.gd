ee6234cca23927f693d398f41faaec7980c63afd
krisp3t <77499501+krisp3t@users.noreply.github.com>
2024-01-27 16:42:37 +0100

    Add ProgessBars

--- scenes/Level.gd
+++ scenes/Level.gd
@@ -4,4 +4,20 @@ func _ready():
 	%LaughProgressBar.value = 50.0;
 
 func _process(delta):
-	pass
+	%EraserProgressBar.value = %EraserTimer.time_left / %EraserTimer.wait_time;
+	%SpitballProgressBar.value = %SpitballTimer.time_left / %SpitballTimer.wait_time;
+	%LaughProgressBar.value -= 0.9 * delta;
+	
+func _on_eraser_timer_timeout():
+	%EraserButton.disabled = false;
+
+func _on_spitball_timer_timeout():
+	%SpitballButton.disabled = false;
+
+func _on_eraser_button_pressed():
+	%EraserButton.disabled = true;
+	%EraserTimer.start();
+
+func _on_spitball_button_pressed():
+	%SpitballButton.disabled = true;
+	%SpitballButton.start();
