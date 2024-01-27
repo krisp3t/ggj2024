973cc4f3da3cacdae38da0026ce7ebcee518083a
NebulaeOfnir <118994104+NebulaeOfnir@users.noreply.github.com>
2024-01-27 19:57:40 +0100

    Revert "Bla"
    This reverts commit 67ed4750b0a806f9f0a2deb4cc33049f9e6d9558.

--- scenes/Enemy.gd
+++ scenes/Enemy.gd
@@ -3,9 +3,8 @@ extends RigidBody2D
 
 func _ready():
 	set_freeze_enabled(true);
-	
-	$AnimatedSprite2D.sprite_frames = sprite;
 	$AnimatedSprite2D.play();
+	$AnimatedSprite2D.sprite_frames = sprite;
 
 func _process(delta):
 	pass
--- scenes/Level.gd
+++ scenes/Level.gd
@@ -47,7 +47,3 @@ func _on_ground_area_body_entered(body):
 	if body is Projectile:
 		await get_tree().create_timer(0.5).timeout;
 		body.queue_free();
-		
-
-
-
