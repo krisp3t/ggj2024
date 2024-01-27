73dcce14472fe83f6c8151ed66ac54f01f9381fd
krisp3t <77499501+krisp3t@users.noreply.github.com>
2024-01-27 19:19:20 +0100

    Update Enemy

--- scenes/Enemy.gd
+++ scenes/Enemy.gd
@@ -1,8 +1,10 @@
 extends RigidBody2D
+@export var sprite : SpriteFrames;
 
 func _ready():
 	set_freeze_enabled(true);
 	$AnimatedSprite2D.play();
+	$AnimatedSprite2D.sprite_frames = sprite;
 
 func _process(delta):
 	pass
