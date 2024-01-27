bf79e92d4516d8f6cdd149fd82b37e01d3c06a1c
krisp3t <77499501+krisp3t@users.noreply.github.com>
2024-01-28 00:02:18 +0100

    Add raycasts

--- scenes/Teacher.gd
+++ scenes/Teacher.gd
@@ -1,10 +1,40 @@
 extends RigidBody2D
-@export var sprite : SpriteFrames;
+var direction : Direction = Direction.FORWARD;
+
+enum Direction {
+	FORWARD,
+	LEFT,
+	RIGHT
+}
 
 func _ready():
 	set_freeze_enabled(true);
-	$AnimatedSprite2D.sprite_frames = sprite;
-	$AnimatedSprite2D.play();
+	$AnimatedSprite2D.play("forward");
 
 func _process(delta):
-	pass
+	match direction:
+		Direction.LEFT:
+			position.x -= 10 * delta;
+		Direction.RIGHT:
+			position.x += 10 * delta;
+
+func _on_raycast_timer_timeout():
+	var x : int = randi_range(-500, 500);
+	var pos : Vector2 = Vector2(x, $RayCast2D.target_position.y);
+	$RayCast2D.target_position = pos;
+	$Line2D.points[1] = pos;
+
+func _on_move_timer_timeout():
+	var i := randi_range(0, 2);
+	var d : Direction;
+	match i:
+		0: 
+			d = Direction.FORWARD;
+			$AnimatedSprite2D.play("forward");
+		1:
+			d = Direction.LEFT;
+			$AnimatedSprite2D.play("left");
+		2:
+			d = Direction.RIGHT;
+			$AnimatedSprite2D.play("right");
+	
