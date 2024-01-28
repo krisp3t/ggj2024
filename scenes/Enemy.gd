1f2c09a9497048e84e36fcd56d15aa9fe52b026a
krisp3t <77499501+krisp3t@users.noreply.github.com>
2024-01-28 14:01:11 +0100

    Add enemy collision detection

--- scenes/Enemy.gd
+++ scenes/Enemy.gd
@@ -1,11 +1,19 @@
 extends RigidBody2D
+class_name Enemy
+
 @export var sprite : SpriteFrames;
-@export var laugh_value : int
+@export var laugh_value : int;
+@onready var game_manager = get_node("/root/GameManager");
 
 func _ready():
 	set_freeze_enabled(true);
 	$AnimatedSprite2D.sprite_frames = sprite;
 	$AnimatedSprite2D.play();
+	
+
 
 func _process(delta):
 	pass
+
+
+		
--- scenes/Projectile.gd
+++ scenes/Projectile.gd
@@ -1,6 +1,7 @@
 extends RigidBody2D
 class_name Projectile;
 @export var texture_path : String = "res://assets/sprites/png/spitball.png";
+@onready var game_manager = get_node("/root/GameManager");
 
 func _ready():
 	set_freeze_enabled(true);
@@ -17,3 +18,7 @@ func throw():
 	
 func _on_visible_on_screen_notifier_2d_screen_exited():
 	queue_free();
+
+func _on_body_entered(body):
+	if body is Enemy:
+		game_manager.laugh_percentage += body.laugh_value;
